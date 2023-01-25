1e391941b6105e64fbc0a6839622d4067b31f1ce
d.kotwal <deeptikotwal@chevron.com>
2023-01-25 14:09:45 +0530

    nfc tag nfts generated and compiled on fvm

---
+++ EHRs/dicom-optimism-marketplace/nfc-tag-nft-fvm/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
